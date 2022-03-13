00974a8963457096badba8527b650a237a0f516f
Phoenixfan <62504743+Phoenixfan@users.noreply.github.com>
2022-03-13 19:53:59 +0100

    changed my password xD, added gitignore

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 export function call() {
-    //mongoose.connect('mongodb+srv://###:###@###.###.mongodb.net/', {dbName: "###"});
-    mongoose.connect('mongodb+srv://me:Go9dyAtUHr0m400X@chat-backend.cy6ld.mongodb.net/?retryWrites=true&w=majority', {dbName: "chat"});
+    mongoose.connect('mongodb+srv://###:###@###.###.mongodb.net/', {dbName: "###"});
 }
